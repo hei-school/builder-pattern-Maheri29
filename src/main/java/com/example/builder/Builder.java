ab9b00d613a66ff09c7c6a2f3459db66b03c7a57
Maheri29 <hei.mahery.2@gmail.com>
2024-02-05 16:10:48 +0100

    Revert "feat: implement builder pattern"
    This reverts commit 6191e3ecdae84286e98418d09bd697bec95e59ae.

--- src/main/java/com/example/builder/Builder.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.example.builder;
-
-public interface Builder {
-    void reset();
-    void setSeats(Integer number);
-    void setEngine(Engine engine);
-    void setTripComputer(boolean tripComputer);
-    void setGPS(boolean gps);
-}
